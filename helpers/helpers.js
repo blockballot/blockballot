cb9729cdac89e93fc484e804bc3bc0d1d1dda402
Evaline <evalinebai@gmail.com>
2018-03-16 19:35:38 -0700

    Create password generator and install crypto libraries

---
+++ helpers/helpers.js
@@ -0,0 +1,10 @@
+const SHA256 = require('crypto-js/SHA256');
+const rn = require('random-number');
+const rw = require('random-words');
+
+let createPassword = () => {
+  let string = rn() + rw();
+  console.log(string);
+  let pw = SHA256(string).toString();
+  return pw;
+};
