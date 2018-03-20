ffca76127e1ca0b8b6d0e08a27e7660d82a18188
paula-o <paulaobler@gmail.com>
2018-03-20 12:45:32 -0700

    Fixed sign up component card header

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -108,7 +108,7 @@ class Signup extends React.Component {
         <Card>
           <CardHeader
             titleStyle={{marginLeft: 20, marginTop: 10, fontSize: 25}}
-            title="Log in"
+            title="Create your Account"
           />
 
           <CardText style={{marginLeft: 20}}>
