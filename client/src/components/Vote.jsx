ecdc0a3048074227ad1f5eff920f21208dd6c8c0
W <michael.shin.hrr@gmail.com>
2018-03-19 17:00:16 -0700

    rebase2

--- client/src/components/Vote.jsx
+++ client/src/components/Vote.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import PasswordInput from 'grommet/components/PasswordInput';
+import Checkbox from 'material-ui/Checkbox';
 
 class Vote extends React.Component {
   constructor(props) {
@@ -9,46 +9,70 @@ class Vote extends React.Component {
       uniqueId: '',
       isLogin: false,
       isVoteSubmitted: false,
-      isBallotCompleted: false
+      isBallotCompleted: false,
+      ballotName: 'ballot name from database', // database input will replace 
+      ballotOption: [  // database input will replace 
+        { optionName:'question1',
+          optionAnswer: false
+        },
+        { optionName:'question2',
+        optionAnswer: true
+        },
+        { optionName:'question3',
+        optionAnswer: false
+        }
+      ]
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.updateCheck = this.updateCheck.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({
-        uniqueId: event.target.value
-    });
+  componentWillMount() {
+    // get the ballotName and ballot question to update the state
+  } 
+
+  updateCheck(event) {
+    // this.setState((oldState) => {
+    //   return {
+    //     checked: !oldState.checked,
+    //   };
+    // });
   }
 
   handleSubmit(event) {
-    event.preventDefault();
-    axios({
-      method: 'POST',
-      url: '/api/Voter',
-      data: {
-        uniqueId: this.state.uniqueId
-      }
-    })
-    .then(function (res) {
-      console.log('found unique ID', res);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    // event.preventDefault();
+    // axios({
+    //   method: 'POST',
+    //   url: '/api/Voter',
+    //   data: {
+    //     uniqueId: this.state.uniqueId
+    //   }
+    // })
+    // .then(function (res) {
+    //   console.log('found unique ID', res);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
   }
 
   render() {
+    let ballotInfo = this.state;
+    let ballotQuestionList = ballotInfo.ballotOption.map((option, index) => {
+      return  <Checkbox className="checkbox" labelPosition="left" key={index} label={option.optionName} checked={option.optionAnswer} onCheck={this.updateCheck}/>
+    })
+
     return (
       <form>
         <label>
-          <div>Ballot Name</div>
+          <div>VOTE PAGE</div>
+          <div>{ballotInfo.ballotName}</div>
+          <div className="block" >{ballotQuestionList}</div>
         </label>
         <input type="submit" value="Submit" onClick={this.handleSubmit} />
       </form>
     )
   }
-
 }
 
 export default Vote;
