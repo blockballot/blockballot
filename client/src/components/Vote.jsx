f01f7dbfefb5fa787e3cd6cded1f7a4737e11f12
Evaline <evalinebai@gmail.com>
2018-03-20 23:27:11 -0700

    Pass blockchain data to vote component and create helper function to connect web3

--- client/src/components/Vote.jsx
+++ client/src/components/Vote.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+import { default as Web3} from 'web3';
+import getWeb3 from '../../../helpers/getWeb3.js';
+import contract from 'truffle-contract';
 import VoteResults from './VoterResults.jsx';
+import TestVote from '../../../build/contracts/TestVote.json';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import { RaisedButton, Checkbox} from 'material-ui';
 import '../style/voter.css';
 
@@ -10,11 +16,16 @@ class Vote extends React.Component {
     super(props);
     this.state = {
       uniqueId: '',
-      isLogin: false,
+      isLoggedIn: false,
+      web3: null,
+      storageValue: 0,
+      contractInstance: null,
+      voteHash: null,
+      candidateName: 'Evaline',
       isVoteSubmitted: false,
       isBallotCompleted: false,
-      ballotName: 'what is your favorite color', // database input will replace 
-      ballotOption: [  // database input will replace 
+      ballotName: 'what is your favorite color', // database input will replace
+      ballotOption: [  // database input will replace
         { optionName:'red',
           optionAnswer: false
         },
@@ -27,12 +38,21 @@ class Vote extends React.Component {
       ]
     };
     this.updateCheck = this.updateCheck.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.submitVote = this.submitVote.bind(this);
   }
 
   componentWillMount() {
-    // get the ballotName and ballot question to update the state
-  } 
+    getWeb3
+    .then(results => {
+      this.setState({
+        web3: results.web3
+      })
+      this.instantiateContract()
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
 
   updateCheck(event) {
     for (var i = 0; i < this.state.ballotOption.length; i++) {
@@ -46,25 +66,37 @@ class Vote extends React.Component {
     }
   }
 
-  handleSubmit(event) {
-    // event.preventDefault();
-    // axios({
-    //   method: 'POST',
-    //   url: '/api/Voter',
-    //   data: {
-    //     uniqueId: this.state.uniqueId
-    //   }
-    // })
-    // .then(function (res) {
-    //   console.log('found unique ID', res);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-    this.setState ({
-      isVoteSubmitted: true
-    })
+  instantiateContract() {
+    const TestContract = contract(TestVote);
+    TestContract.setProvider(this.state.web3.currentProvider);
 
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      TestContract.deployed().then((instance) => {
+        this.setState({
+          contractInstance: instance
+        });
+      });
+    });
+  }
+
+  submitVote(candidate) {
+    // Replace state variable with candidate param onclick
+    let candidateName = this.state.candidateName;
+    console.log(candidateName);
+    var TestContractInstance;
+
+    TestContractInstance = this.state.contractInstance;
+
+    TestContractInstance.voteForCandidate(candidateName, {gas: 2800000, from: this.state.web3.eth.accounts[0]})
+    .then((result) => {
+      this.setState({
+        voteHash: result.tx
+      });
+      return TestContractInstance.totalVotesFor.call(candidateName)
+    }).then((voteCount) => {
+      console.log(voteCount);
+      this.setState({storageValue: voteCount.c[0]})
+    });
   }
 
   render() {
@@ -72,7 +104,7 @@ class Vote extends React.Component {
     let ballotQuestionList = ballotInfo.ballotOption.map((option, index) => {
       return  <Checkbox className="checkbox" labelPosition="left" key={index} label={option.optionName} checked={option.optionAnswer} onCheck={this.updateCheck} name={option.optionName}/>
     })
-    
+
     if(this.state.isVoteSubmitted === true) {
       return (
         <VoteResults ballotOption={this.state.ballotOption} ballotName= {this.state.ballotName} />
@@ -85,7 +117,7 @@ class Vote extends React.Component {
             <div>{ballotInfo.ballotName}</div>
             <div className="block" >{ballotQuestionList}</div>
           </label>
-          <RaisedButton label="Submit" type="submit" value="Submit" onClick={this.handleSubmit} />
+          <RaisedButton label="Submit" onClick={this.submitVote} />
         </form>
       )
     }
