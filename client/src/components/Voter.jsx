92be112983a6e02ac9cf5920527f8036a6628dc1
Evaline <evalinebai@gmail.com>
2018-03-30 20:46:17 -0700

    Code cleanup

--- client/src/components/Voter.jsx
+++ client/src/components/Voter.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import Vote from './Vote.jsx';
-import VoteResults from './VoterResults.jsx';
 import { Card, TextField } from 'material-ui';
 import { Button } from 'semantic-ui-react';
+import Vote from './Vote';
 import '../style/voter.css';
 
 
@@ -13,10 +12,10 @@ class Voter extends React.Component {
     this.state = {
       uniqueId: '',
       isLogin: false,
-      pollId: 0, 
+      pollId: 0,
       errorText: '',
       pollHash: '',
-      pollEnd: ''
+      pollEnd: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,80 +23,79 @@ class Voter extends React.Component {
 
   handleChange(event) {
     this.setState({
-        uniqueId: event.target.value
+      uniqueId: event.target.value,
     });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    var voter = this;
+    const voter = this;
     axios({
       method: 'POST',
       url: '/api/voter',
       data: {
-        uniqueId: this.state.uniqueId
+        uniqueId: this.state.uniqueId,
       }
     })
-    .then((res) => {
-      const poll = res.data.pollId;
-      const hash = res.data.poll.pollHash;
-      const endTime = res.data.poll.pollTimeEnd;
-      voter.setState({
-        isLogin: true,
-        pollId: poll,
-        pollHash: hash,
-        pollEnd: endTime
+      .then((res) => {
+        const poll = res.data.pollId;
+        const hash = res.data.poll.pollHash;
+        const endTime = res.data.poll.pollTimeEnd;
+        voter.setState({
+          isLogin: true,
+          pollId: poll,
+          pollHash: hash,
+          pollEnd: endTime,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        voter.setState({
+          errorText: 'Your unique code is incorrect. Please try again',
+        });
       });
-    })
-    .catch((error) => {
-      console.log(error);
-      voter.setState({
-        errorText: "Your unique code is incorrect. Please try again"
-      });
-    });
   }
 
   render() {
-    if(this.state.isLogin) {
+    if (this.state.isLogin) {
       return (
-        <Vote 
+        <Vote
           pollId={this.state.pollId}
           pollHash={this.state.pollHash}
           pollEnd={this.state.pollEnd}
         />
-      )
-    } else {
-      return (
-        <div>
-          <div className='header'>
-          Enter Your Voter Code
-          </div>
-            <form>
-              <Card className='enterCode'>
-                <TextField
-                  type='password'
-                  name='uniqueId'
-                  hintText='Enter Code'
-                  value={this.state.uniqueId}
-                  onChange={this.handleChange}
-                  errorText= {this.state.errorText}
-                  underlineFocusStyle={{ borderBottomColor: '#4183D9' }}
-                />
-                <br/>
-                <Button
-                  primary
-                  className='submitButton'
-                  type='submit'
-                  value='Submit'
-                  onClick={this.handleSubmit}
-                >
-                  Submit
-                </Button>
-              </Card>
-            </form>
-        </div>
-      )
+      );
     }
+    return (
+      <div>
+        <div className="header">
+        Enter Your Voter Code
+        </div>
+        <form>
+          <Card className="enterCode">
+            <TextField
+              type="password"
+              name="uniqueId"
+              hintText="Enter Code"
+              value={this.state.uniqueId}
+              onChange={this.handleChange}
+              errorText={this.state.errorText}
+              underlineFocusStyle={{ borderBottomColor: '#4183D9' }}
+            />
+            <br />
+            <Button
+              primary
+              className="submitButton"
+              type="submit"
+              value="Submit"
+              onClick={this.handleSubmit}
+            >
+              Submit
+            </Button>
+          </Card>
+        </form>
+      </div>
+    );
   }
 }
 
