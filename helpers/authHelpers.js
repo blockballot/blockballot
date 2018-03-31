d3713c2c6b843ee2b1f032d4d9e929578a69e5b8
Norbie Magno <nmagno@gmail.com>
2018-03-31 15:38:44 -0700

    Fix username not properly rendering on dashboard.

--- helpers/authHelpers.js
+++ helpers/authHelpers.js
@@ -1,13 +1,10 @@
 const bcrypt = require('bcrypt');
-const Promise = require('bluebird');
-const request = require('request');
-const db = require('../database/index.js');
 
-let isLoggedIn = function(req) {
+const isLoggedIn = (req) => {
   return req.session ? !!req.session.user : false;
 };
 
-let checkUser = (req, res, next) => {
+const checkUser = (req, res, next) => {
   if (!isLoggedIn(req)) {
     res.redirect('/login');
   } else {
@@ -15,19 +12,18 @@ let checkUser = (req, res, next) => {
   }
 };
 
-let createSession = (req, res, newUser) => {
+const createSession = (req, res, newUser) => {
   return req.session.regenerate(() => {
     res.cookie('loggedIn', 'true', { maxAge: 60 * 60 * 1000 });
-    res.cookie('username', newUser.dataValues.orgEmail, { maxAge: 60 * 60 * 1000 });
-    req.session.user = newUser.dataValues.orgEmail;
+    res.cookie('username', newUser.dataValues.orgName, { maxAge: 60 * 60 * 1000 });
+    req.session.user = newUser.dataValues.orgName;
     req.session.orgId = newUser.dataValues.id;
-    console.log(req.session);
     res.status(200).send();
   });
 };
 
-let comparePassword = (attemptedPassword, org, callback) => {
-  let existingPassword = org.orgPassword;
+const comparePassword = (attemptedPassword, org, callback) => {
+  const existingPassword = org.orgPassword;
   bcrypt.compare(attemptedPassword, existingPassword, (err, isMatch) => {
     callback(isMatch);
   });
