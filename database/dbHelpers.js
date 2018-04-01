fa3baa35689a45bdb8801d8177bc934097b7813a
W <michael.shin.hrr@gmail.com>
2018-03-31 23:31:37 -0700

    updated dbhelper retrievepolls to include pollExpired

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -27,7 +27,7 @@ const saveVoterID = (voteID, pollId) => {
 const retrievePolls = (orgId) => {
   return new Promise((resolve, reject) => {
     db.sequelize.query(`SELECT p.pollName, DATE_FORMAT(p.pollTimeStart,  '%Y-%m-%d %h:%i %p') as pollTimeStart, 
-                        DATE_FORMAT(p.pollTimeEnd,  '%Y-%m-%d %h:%i %p') as pollTimeEnd, p.pollHash,
+                        DATE_FORMAT(p.pollTimeEnd,  '%Y-%m-%d %h:%i %p') as pollTimeEnd, p.pollHash, p.pollExpired,
                         p.orgId, GROUP_CONCAT(o.optionName) as options,
                         GROUP_CONCAT(o.id) as optionIds, o.pollId
                         FROM polls p
