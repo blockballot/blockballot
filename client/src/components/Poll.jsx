85fafcc4190c754a6a25d4783dbe8dbc8e7470ea
W <michael.shin.hrr@gmail.com>
2018-03-31 21:57:37 -0700

    updated the poll card start & end time for manual startNow

--- client/src/components/Poll.jsx
+++ client/src/components/Poll.jsx
@@ -4,6 +4,21 @@ import PollResults from './PollResults.jsx';
 import {Link, Redirect, Route} from 'react-router-dom';
 
 const Poll = (props) => {
+  let time = null;
+  if (props.poll.pollTimeStart === null && props.poll.pollTimeEnd === null) {
+    time = (
+      <div>
+        <b>Manual</b>
+      </div>
+    )
+  } else {
+    time = (
+      <div>
+        <b>Start: </b>{props.poll.pollTimeStart} (GMT) <br/>
+        <b>End: </b>{props.poll.pollTimeEnd} (GMT)
+      </div>
+    )
+  }
   return (
       <div className="card"
       onClick={() => props.handlePollClick(props.poll)}>
@@ -11,13 +26,11 @@ const Poll = (props) => {
           <div className="header">
             {props.poll.pollName}
           </div>
-
           <br/>
-          
+          {time}
           <div className="extra content">
             <span className="left floated">
-              <b>Start: </b>{props.poll.pollTimeStart} (GMT) <br/>
-              <b>End: </b>{props.poll.pollTimeEnd} (GMT)
+         
             </span>
             <span className="right floated">
               {props.poll.voteCount}
