19e3ade1b4ab163bac306165d893205232827b2b
paula-o <paulaobler@gmail.com>
2018-04-02 00:40:39 -0700

    Update Reset Password Template

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -2,8 +2,21 @@ const nodemailer = require('nodemailer');
 const env = require('dotenv').config();
 const helpers = require('../helpers/helpers.js');
 const dbHelper = require('../database/dbHelpers.js');
+const fs = require('fs');
+const handlebars = require('handlebars');
+const path = require('path');
 
-let transporter = nodemailer.createTransport({
+
+const readHTMLFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, {encoding: 'utf-8'}, function(err, file) {
+      if (err) { reject(err) } 
+      else { resolve(file) }
+    })  
+  })
+};
+
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   port: 587,
   secure: false, 
@@ -13,28 +26,58 @@ let transporter = nodemailer.createTransport({
   }
 });
 
-const sendPasswordReset = function(email, token) {
-  return new Promise(function(resolve, reject) {
-    let mailPasswordOptions = {
-      from: '"BlockBallot" <blockballot@gmail.com>', 
-      to: `${email}`, 
-      subject: 'Link to reset your password', 
-      text: 'You are receiving this email because you have requested to reset your password.' + '\n' +
-      'Please click on the link below to complete the process.' + '\n' +
-      'http://localhost:3000' + '/reset/' + token + '\n' +
-      'If you did not request this, please ignore this message and your password will remain unchanged.'
-    };  
+const sendPasswordReset = (email, token) => {
+  return new Promise((resolve, reject) => {
+    readHTMLFile(path.join(__dirname, '../client/src/templates/resetPasswordEmail.html'))
+    .then(template => {
+      let compiler = handlebars.compile(template);
+      let replacements = {
+        resetLink: 'http://localhost:3000' + '/reset/' + token
+      };
+      let templateToSend = compiler(replacements);
+      let mailPasswordOptions = {
+        from: '"BlockBallot" <blockballot@gmail.com>', 
+        to: `${email}`, 
+        subject: 'Link to reset your password', 
+        html: templateToSend
+      }; 
 
-    transporter.sendMail(mailPasswordOptions, (error, info) => {
-      if (error) {
-        reject(error);
-      } else {
-        console.log('no error');
-        resolve(info);
-      }
-    });
+      transporter.sendMail(mailPasswordOptions, (error, info) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(info);
+        }
+      });
+    }) 
   })
 }
+
+
+
+
+// const sendPasswordReset = function(email, token) {
+//   return new Promise(function(resolve, reject) {
+//     let mailPasswordOptions = {
+//       from: '"BlockBallot" <blockballot@gmail.com>', 
+//       to: `${email}`, 
+//       subject: 'Link to reset your password', 
+//       text: 'You are receiving this email because you have requested to reset your password.' + '\n' +
+//       'Please click on the link below to complete the process.' + '\n' +
+//       'http://localhost:3000' + '/reset/' + token + '\n' +
+//       'If you did not request this, please ignore this message and your password will remain unchanged.'
+//     };  
+
+//     transporter.sendMail(mailPasswordOptions, (error, info) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         console.log('no error');
+//         resolve(info);
+//       }
+//     });
+//   })
+// }
 
 const sendEmailCodes = (emails, pollId) => {
   return new Promise(function(resolve, reject) {
